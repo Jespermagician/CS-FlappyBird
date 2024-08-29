fb569285e737a1c430d481dbbe3679fa9174cc06
jespermagician <jesper@herlings.de>
2024-08-29 22:00:21 +0200

    Add Hello World and Readme

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlappyBird
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt");
+        }
+    }
+}
